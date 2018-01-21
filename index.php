3e8016052a2c210143b42c5c57e0d93a7a3a4f44
officeCFH <15364847333@qq.com>
2018-01-21 17:15:41 +0800

    增加php代码片断显示方法

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <?php
-highlight_string("<?php echo \"hello!\" ?>");
-
 function HeighPHPcode($Heightstring){
-  do{
-     $z=0;
-     if(preg_match('/<phpcode>(.*?)</phpcode>/s',$Heightstring,$reg)) {
-       $z=1;
+     if($Heightstring) {
        $code='';
        ob_start();
-       highlight_string($reg[1]);
-       $code = ob_get_contents(); 
+       highlight_string($Heightstring);
+       $code = ob_get_contents();
        ob_end_clean();
-       $reg[1] = addslashes($reg[1]);
-       $Heightstring=preg_replace("/<phpcode>(.*?)</phpcode>/s","$code",$Heightstring,1);
-      }
-  }while($z);
-  return $Heightstring;
+       $Heightstring=$code;
+     }
+     return $Heightstring;
 }
-
+$aa=HeighPHPcode("<?php echo 'ok';?>");
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div>
+      <?php echo $aa;?>
+    </div>
+  </body>
+</html>
